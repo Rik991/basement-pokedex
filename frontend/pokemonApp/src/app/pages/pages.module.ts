05e15b00f761dbf0dce1ec50ffc6c5f885b416cc
Riccardo Santilli <riccardosantilli@live.it>
2025-03-18 17:11:28 +0100

    added pokemon-types page, task 2 completed

--- frontend/pokemonApp/src/app/pages/pages.module.ts
+++ frontend/pokemonApp/src/app/pages/pages.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { PokemonTypeComponent } from './pokemon-type/pokemon-type.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'pokemon-type/:type', component: PokemonTypeComponent },
+];
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, PokemonTypeComponent],
   imports: [CommonModule, FormsModule, RouterModule.forChild(routes)],
 })
 export class PagesModule {}
---
+++ frontend/pokemonApp/src/app/pages/pokemon-type/pokemon-type.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { IPokemonType } from '../../interfaces/i-pokemon-type';
+import { PokemonService } from '../../services/pokemon.service';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-pokemon-type',
+  templateUrl: './pokemon-type.component.html',
+  styleUrl: './pokemon-type.component.scss',
+})
+export class PokemonTypeComponent {
+  type: string = '';
+  pokemonTypeList: IPokemonType | null = null;
+  errorMessage: string = '';
+
+  constructor(
+    private pokemonSvc: PokemonService,
+    private route: ActivatedRoute
+  ) {}
+
+  //nell'ngonInit recupero il tipo scelto dal parametro url e lo do alla funzione dedicata
+  ngOnInit() {
+    this.route.params.subscribe((params) => {
+      this.type = params['type'];
+      this.getPokemonByType(this.type);
+    });
+  }
+
+  //task 2, metodo per ricavare la lista dei pokemon di un determinato tipo, da backend ci tornano 10 item per tipo
+  getPokemonByType(type: string): void {
+    this.pokemonSvc.getPokemonByType(type).subscribe({
+      next: (pokemonList) => {
+        this.pokemonTypeList = pokemonList;
+      },
+      error: (error) => {
+        console.error('Errore nel recupero dei Pokémon per tipo', error);
+        this.errorMessage =
+          'Errore nel recupero dei Pokémon per il tipo selezionato.';
+      },
+    });
+  }
+}
